4b76540decd97933778669772aa424ca4f66914e
Dongha922 <kdh4575@naver.com>
2022-11-15 07:39:46 +0900

    이미지 슬라이더완

---
+++ guestshop/client/src/components/utils/ImageSlider.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Icon, Col, Card, Row, Carousel } from "antd";
+
+function ImageSlider(props) {
+  return (
+    <div>
+      <Carousel autoplay>
+        {props.images.map((image, index) => (
+          <div key={index}>
+            <img
+              style={{
+                height: "190px",
+              }}
+              src={`http://localhost:5000/${image}`}
+            />
+          </div>
+        ))}
+      </Carousel>
+    </div>
+  );
+}
+
+export default ImageSlider;
--- guestshop/client/src/components/views/LandingPage/LandingPage.js
+++ guestshop/client/src/components/views/LandingPage/LandingPage.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Icon, Col, Row, Card } from "antd";
 import Meta from "antd/lib/card/Meta";
+import ImageSlider from "../../utils/ImageSlider";
+
 function LandingPage() {
   const [Products, setProducts] = useState([]);
 
@@ -18,18 +20,11 @@ function LandingPage() {
   }, []);
 
   const renderCards = Products.map((product, index) => {
-    console.log("product", product);
+    //console.log("product", product);
 
     return (
       <Col lg={6} md={8} xs={24} key={index}>
-        <Card
-          cover={
-            <img
-              style={{ width: "100%", height: "150px" }}
-              src={`http://localhost:5000/${product.images[0]}`}
-            />
-          }
-        >
+        <Card cover={<ImageSlider images={product.images} />}>
           <Meta title={product.title} description={`${product.price} `} />
         </Card>
       </Col>
